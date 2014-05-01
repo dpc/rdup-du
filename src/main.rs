846d710c766bf75d5134a79df57456706869bbb2
Dawid Ciężarkiewicz <dpc@ucore.info>
2014-05-01 00:08:03 -0700

    Handle io errors.

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ use std::cmp::{Ordering,Less,Equal,Greater};
 use std::io;
 use std::io::fs;
 use std::io::fs::lstat;
+use std::io::IoError;
+use std::io::stdio::stderr;
+use std::io::TypeSymlink;
+use std::num::pow;
 use std::os;
 use std::path::Path;
-use std::num::pow;
-use std::io::TypeSymlink;
 
 struct SizeSortedFile {
 	entry : Path,
@@ -64,49 +66,79 @@ fn format_size(size : u64) -> ~str {
 	format!("{:>6}{}", size / pow(1024u64, i), postfixes[i])
 }
 
-fn visit_dirs(dir: &Path, cb: |&Path|) -> io::IoResult<()> {
+fn visit_dirs(dir: &Path, cb: |&Path|, err: |&Path, IoError|) {
 	let mut nobackup = dir.clone();
 	nobackup.push(".nobackup");
 
 	if nobackup.is_file() {
 		cb(&nobackup);
-		return Ok(())
+		return;
 	}
 
 	if dir.is_dir() {
-		let contents = try!(fs::readdir(dir));
-		for entry in contents.iter() {
-			if !is_link(entry) && entry.is_dir() {
-				match visit_dirs(entry, |p| cb(p)) {
-					Err(x) => return Err(x),
-					Ok(_) => {},
-				};
-			} else {
-				cb(entry)
+		let contents = fs::readdir(dir);
+		match contents {
+			Ok(contents) => {
+				for entry in contents.iter() {
+					if !is_link(entry) && entry.is_dir() {
+						visit_dirs(entry, |p| cb(p), |p, e| err(p, e));
+					} else {
+						cb(entry);
+					}
+				}
+			},
+			Err(e) => {
+				err(dir, e);
 			}
-		}
-		Ok(())
+		};
 	} else {
-		Err(io::standard_error(io::InvalidInput))
+		err(dir, io::standard_error(io::InvalidInput));
 	}
 }
 
 
+fn print_error_path(p : &Path, e : IoError) {
+	(writeln!(&mut stderr(), "{}: {}", p.display(), e)).unwrap();
+}
+
 fn visit_dirs_summary(dir: &Path) {
 	let mut entries = TreeSet::<SizeSortedFile>::new();
 
-	for entry in fs::readdir(dir).unwrap().iter() {
-		let size = if !is_link(entry) && entry.is_dir() {
-			let mut size = 0;
-			visit_dirs(entry, |p| if p.is_file() {
-				size = size + lstat(p).unwrap().size
-			} ).unwrap();
-			size
-		} else {
-			lstat(entry).unwrap().size
-		};
-
-		entries.insert(SizeSortedFile{entry: entry.clone(), size: size});
+	match fs::readdir(dir) {
+		Ok(dir) => {
+			for entry in dir.iter() {
+				let size = if !is_link(entry) && entry.is_dir() {
+					let mut size = 0;
+					visit_dirs(entry, |p| if p.is_file() {
+						match lstat(p) {
+							Ok(lstat) => {
+								size = size + lstat.size;
+							},
+							Err(err) => {
+								print_error_path(entry, err);
+							}
+						}
+					},
+					print_error_path
+					);
+					size
+				} else {
+					match lstat(entry) {
+							Ok(lstat) => {
+								lstat.size
+							},
+							Err(err) => {
+								print_error_path(entry, err);
+								0
+							}
+						}
+				};
+				entries.insert(SizeSortedFile{entry: entry.clone(), size: size});
+			}
+		},
+		Err(err) => {
+			print_error_path(dir, err);
+		}
 	};
 
 	let mut total_size = 0;
