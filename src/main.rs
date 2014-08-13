3fbd93e8070d092d970125e1104eecffb26b1a46
Dawid Ciężarkiewicz <dpc@ucore.info>
2014-08-12 23:18:05 -0700

    Implement `-x`
    Fixes #1.

--- src/main.rs
+++ src/main.rs
@@ -60,6 +60,32 @@ fn is_link(p : &Path) -> Result<bool,IoError> {
 	})
 }
 
+// Is the entry a dir, that we should recursively traverse?
+fn should_visit(p : &Path, fs : Option<u64>) -> bool {
+	if !is_link(p).unwrap_or_else(
+		|e| {print_error_path(p, e); false}
+		) && p.is_dir() {
+		if fs.is_some() {
+			let stat = match fs::stat(p) {
+				Err(e) => {
+					print_error_path(p, e);
+					return false;
+				},
+				Ok(stat) => stat
+			};
+			if fs != Some(stat.unstable.device) {
+				false
+			} else {
+				true
+			}
+		} else {
+			true
+		}
+	} else {
+		false
+	}
+}
+
 fn bytes_to_humanreadable(size : u64) -> String {
 	let postfixes = ["B", "K", "M", "G", "T"];
 
@@ -73,11 +99,11 @@ fn bytes_to_humanreadable(size : u64) -> String {
 	format!("{}{}", size / pow(1024u64, i), postfixes[i])
 }
 
-fn bytes_to_str(size : u64) -> String {
+fn bytes_to_string(size : u64) -> String {
 	format!("{:>12}", size)
 }
 
-fn visit_dirs(dir: &Path, cb: |&Path|, err: |&Path, IoError|) {
+fn visit_dirs(dir: &Path, fs : Option<u64>, cb: |&Path|, err: |&Path, IoError|) {
 	let mut nobackup = dir.clone();
 	nobackup.push(".nobackup");
 
@@ -91,10 +117,8 @@ fn visit_dirs(dir: &Path, cb: |&Path|, err: |&Path, IoError|) {
 		match contents {
 			Ok(contents) => {
 				for entry in contents.iter() {
-					if !is_link(entry).unwrap_or_else(
-						|e| {print_error_path(entry, e); false}
-						) && entry.is_dir() {
-						visit_dirs(entry, |p| cb(p), |p, e| err(p, e));
+					if should_visit(entry, fs) {
+						visit_dirs(entry, fs, |p| cb(p), |p, e| err(p, e));
 					} else {
 						cb(entry);
 					}
@@ -113,25 +137,40 @@ fn print_error_path(p : &Path, e : IoError) {
 	(writeln!(&mut stderr(), "{}: {}", p.display(), e)).unwrap();
 }
 
-fn visit_dirs_summary(dir: &Path, size_f : |u64| -> String) {
+fn visit_dirs_summary(dir: &Path, config : &CmdConfig) {
 	let mut entries = TreeSet::<SizeSortedFile>::new();
+
+        let size_f = match config.bytes {
+            true => bytes_to_string,
+            false => bytes_to_humanreadable
+        };
+
+	let fs = if config.localfs {
+		match fs::stat(dir) {
+			Err(e) => {
+				print_error_path(dir, e);
+				return;
+			},
+			Ok(stat) => Some(stat.unstable.device)
+		}
+	} else { None };
 
 	match fs::readdir(dir) {
 		Ok(dir) => {
 			for entry in dir.iter() {
-				let size = if !is_link(entry).unwrap_or_else(
-						|e| {print_error_path(entry, e); true}
-						) && entry.is_dir() {
+
+
+				let size = if should_visit(entry, fs) {
 					let mut size = 0;
-					visit_dirs(entry, |p| if p.is_file() {
+					visit_dirs(entry, fs, |p| if p.is_file() {
 
 						lstat(p)
 							.map(|l| size = size + l.size)
 							.unwrap_or_else(
 								|e| {print_error_path(p, e)}
 								)
-					},
-					print_error_path
+						},
+						print_error_path
 					);
 					size
 				} else {
@@ -162,7 +201,13 @@ fn visit_dirs_summary(dir: &Path, size_f : |u64| -> String) {
 fn print_usage(program: &str) {
 	println!("Usage: {} [options]", program);
 	println!("-b --bytes\tPrint size in bytes");
+	println!("-x --localfs\tStay on local filesystem");
 	println!("-h --help\tUsage");
+}
+
+struct CmdConfig {
+	localfs : bool,
+	bytes : bool
 }
 
 fn main() {
@@ -172,7 +217,8 @@ fn main() {
 
 	let opts = [
 		optflag("b", "bytes", "print size in bytes"),
-		optflag("h", "help", "print this help menu")
+		optflag("h", "help", "print this help menu"),
+		optflag("x", "localfs", "stay on local filesystem"),
 			];
 
 	let matches = match getopts(args.tail(), opts) {
@@ -191,9 +237,11 @@ fn main() {
 
 	let d = &std::path::Path::new(dir);
 
-	if matches.opt_present("b") {
-		visit_dirs_summary(d, bytes_to_str);
-	} else {
-		visit_dirs_summary(d, bytes_to_humanreadable);
-	}
+	let conf = CmdConfig {
+		bytes: matches.opt_present("b"),
+		localfs: matches.opt_present("x"),
+	};
+
+	visit_dirs_summary(d, &conf);
 }
+// vim: ts=8 sw=8 noexpandtab
