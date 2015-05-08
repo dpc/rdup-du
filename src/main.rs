fc5c6a26129ed5c5960fe1d30a3c256b03be0713
Dawid Ciężarkiewicz <dawidc@nvidia.com>
2015-05-08 00:55:27 -0700

    Rustc update.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
+#![feature(path_ext)]
+#![feature(metadata_ext)]
+#![feature(file_type)]
+#![feature(raw_ext)]
+#![feature(dir_entry_ext)]
 extern crate getopts;
-extern crate collect;
 
-use collect::TreeSet;
-use getopts::{optflag,getopts};
+use std::collections::BTreeSet;
+use getopts::Options;
 use std::cmp::{Ord,Eq,PartialEq,PartialOrd,Ordering};
 use std::cmp::Ordering::{Less,Equal,Greater};
 use std::io;
-use std::io::fs;
-use std::io::fs::lstat;
-use std::io::IoError;
-use std::io::stdio::stderr;
-use std::io::FileType::Symlink;
-use std::os;
+use std::io::Write;
+use std::fs;
+use std::io::stderr;
 use std::path::Path;
-use std::io::fs::PathExtensions;
+use std::path;
+use std::fs::PathExt;
+use std::os::linux::fs::MetadataExt;
 
 struct SizeSortedFile {
-	entry : Path,
+	entry : path::PathBuf,
 	size : u64,
 }
 
@@ -53,37 +56,28 @@ impl PartialEq for SizeSortedFile {
 
 impl Eq for SizeSortedFile {
 }
-
-fn is_link(p : &Path) -> Result<bool,IoError> {
-	lstat(p).map(|e| match e.kind {
-		Symlink => true,
-		_ => false,
-	})
-}
+/*
+fn is_link(p : &Path) -> Result<bool, io::Error> {
+	let file = try!(fs::File::open(p));
+	let metadata = try!(file.metadata());
+	metadata.file_type().is_symlink()
+}*/
 
 // Is the entry a dir, that we should recursively traverse?
 fn should_visit(p : &Path, fs : Option<u64>) -> bool {
-	if !is_link(p).unwrap_or_else(
-		|e| {print_error_path(p, e); false}
-		) && p.is_dir() {
-		if fs.is_some() {
-			let stat = match fs::stat(p) {
-				Err(e) => {
-					print_error_path(p, e);
-					return false;
-				},
-				Ok(stat) => stat
-			};
-			if fs != Some(stat.unstable.device) {
+	match p.metadata() {
+		Err(e) => { print_error_path(p, e); false },
+		Ok(meta) => {
+			if meta.file_type().is_symlink() {
 				false
+			} else if !meta.file_type().is_dir() {
+				false
+			} else if fs.is_some() {
+				fs == Some(meta.as_raw_stat().st_dev)
 			} else {
 				true
 			}
-		} else {
-			true
 		}
-	} else {
-		false
 	}
 }
 
@@ -107,24 +101,30 @@ fn bytes_to_string(size : u64) -> String {
 	format!("{:>12}", size)
 }
 
-fn visit_dirs(dir: &Path, fs : Option<u64>, cb: |&Path|, err: |&Path, IoError|) {
-	let mut nobackup = dir.clone();
-	nobackup.push(".nobackup");
+fn visit_dirs<F : FnMut(&Path), E : FnMut(&Path, io::Error)>(dir: &Path, fs : Option<u64>, mut cb: F, mut err: E) {
+	let nobackup = dir.clone();
+	nobackup.join(".nobackup");
 
 	if nobackup.is_file() {
-		cb(&nobackup);
+		cb(nobackup);
 		return;
 	}
 
 	if dir.is_dir() {
-		let contents = fs::readdir(dir);
+		let contents = fs::read_dir(dir);
 		match contents {
 			Ok(contents) => {
-				for entry in contents.iter() {
-					if should_visit(entry, fs) {
-						visit_dirs(entry, fs, |p| cb(p), |p, e| err(p, e));
-					} else {
-						cb(entry);
+				for entry in contents {
+					match entry {
+						Ok(entry) => {
+							let entry_path = entry.path();
+							if should_visit(&entry_path, fs) {
+								visit_dirs(&entry_path, fs, |p| cb(p), |p, e| err(p, e));
+							} else {
+								cb(&entry_path);
+							}
+						},
+						Err(e) => err(dir, e),
 					}
 				}
 			},
@@ -133,16 +133,16 @@ fn visit_dirs(dir: &Path, fs : Option<u64>, cb: |&Path|, err: |&Path, IoError|) 
 			}
 		};
 	} else {
-		err(dir, io::standard_error(io::InvalidInput));
+		err(dir, io::Error::new(io::ErrorKind::InvalidInput, dir.to_str().unwrap_or("")));
 	}
 }
 
-fn print_error_path(p : &Path, e : IoError) {
-	(writeln!(&mut stderr(), "{}: {}", p.display(), e)).unwrap();
+fn print_error_path(p : &Path, e : io::Error) {
+	(writeln!(&mut stderr(), "{}: {}", p.to_string_lossy(), e)).unwrap();
 }
 
 fn visit_dirs_summary(dir: &Path, config : &CmdConfig) {
-	let mut entries = TreeSet::<SizeSortedFile>::new();
+	let mut entries = BTreeSet::<SizeSortedFile>::new();
 
 	let size_f : fn (u64) -> String = match config.bytes {
 		true => bytes_to_string,
@@ -150,40 +150,42 @@ fn visit_dirs_summary(dir: &Path, config : &CmdConfig) {
 	};
 
 	let fs = if config.localfs {
-		match fs::stat(dir) {
+		match dir.metadata() {
 			Err(e) => {
 				print_error_path(dir, e);
 				return;
 			},
-			Ok(stat) => Some(stat.unstable.device)
+			Ok(stat) => Some(stat.as_raw_stat().st_dev)
 		}
 	} else { None };
 
-	match fs::readdir(dir) {
-		Ok(dir) => {
-			for entry in dir.iter() {
-
-				let size = if should_visit(entry, fs) {
-					let mut size = 0;
-					visit_dirs(entry, fs, |p| if p.is_file() {
-
-						lstat(p)
-							.map(|l| size = size + l.size)
-							.unwrap_or_else(
-								|e| {print_error_path(p, e)}
-								)
-						},
-						print_error_path
-					);
-					size
-				} else {
-					lstat(entry)
-						.map(|l| l.size)
-						.unwrap_or_else(
-							|e| {print_error_path(entry, e); 0}
-							)
-				};
-				entries.insert(SizeSortedFile{entry: entry.clone(), size: size});
+	match dir.read_dir() {
+		Ok(dir_listing) => {
+			for entry in dir_listing {
+				match entry {
+					Ok(entry) => {
+						let entry_path = entry.path();
+						let size = if should_visit(&entry_path, fs) {
+							let mut size = 0;
+							visit_dirs(&entry_path, fs, |p| if p.is_file() {
+								match p.metadata() {
+									Err(e) => { print_error_path(p, e) }
+									Ok(meta) => { size = size + meta.len() }
+								}
+							},
+							&print_error_path
+							);
+							size
+						} else {
+							match entry.metadata() {
+								Err(e) => { print_error_path(&entry_path, e); 0}
+								Ok(meta) => { meta.len() }
+							}
+						};
+						entries.insert(SizeSortedFile{entry: entry_path.to_path_buf(), size: size});
+					},
+					Err(e) => { print_error_path(dir, e) }
+				}
 			}
 		},
 		Err(err) => {
@@ -215,31 +217,32 @@ struct CmdConfig {
 
 #[allow(dead_code)]
 fn main() {
-	let args: Vec<String> = os::args();
+	let args: Vec<String> = std::env::args().collect();
 
 	let program = args[0].clone();
 
-	let opts = [
-		optflag("b", "bytes", "print size in bytes"),
-		optflag("h", "help", "print this help menu"),
-		optflag("x", "localfs", "stay on local filesystem"),
-			];
+	let mut opts = Options::new();
+	opts.optflag("b", "bytes", "print size in bytes");
+	opts.optflag("h", "help", "print this help menu");
+	opts.optflag("x", "localfs", "stay on local filesystem");
 
-	let matches = match getopts(args.tail(), opts.as_slice()) {
+	let matches = match opts.parse(&args[1..]) {
 		Ok(m) => { m }
 		Err(f) => { panic!(f.to_string()) }
 	};
+
 	if matches.opt_present("h") {
-		print_usage(program.as_slice());
+		print_usage(&program);
 		return;
 	}
+
 	let dir = if !matches.free.is_empty() {
 		matches.free[0].clone()
 	} else {
 		".".to_string()
 	};
 
-	let d = &std::path::Path::new(dir);
+	let d = &std::path::Path::new(&dir);
 
 	let conf = CmdConfig {
 		bytes: matches.opt_present("b"),
